07ef71c06777b624678731605232a37ef2472e89
Your Real Name Here <Your Email Address Here>
2019-12-14 11:15:19 -0700

    1. Set state, useEffect, and axios call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./components/Header.js";
+import axios from 'axios';
+import styled from 'styled-components';
 
 
 export default function App() {
+
+const [characters, setCharacters]= useState([]);
+
+useEffect(() => {
+  axios.get('https://rickandmortyapi.com/api/character/')
+  .then(response => {
+    console.log(response);
+    setCharacters(response.data.results);    
+  })
+  .catch(err => console.log(err))
+}, [])
+
   return (
+  <div>
     <main>
       <Header />
     </main>
+    
+      <div className= "characters-container">
+        <div className= 'characters-info'>
+          <h2>{characters.name}</h2>
+        </div>
+      </div>
+  </div> 
   );
+  
 }
